1a0997e047b7446b4ab5f0851765e8f7cc93ce56
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-05-04 00:50:55 +0300

    add routes for get user by username and get assignment by id

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express, { urlencoded } from 'express';
-const { MongoClient, ServerApiVersion } = require('mongodb');
+import express from 'express';
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 import { Collection } from 'mongodb';
-require('dotenv').config('./env')
+require('dotenv').config('./env');
 
 class Server {
     mdb_username: string = process.env.MDB_USERNAME as string;
@@ -9,10 +9,12 @@ class Server {
     mdb_cluster: string = process.env.MDB_CLUSTER as string;
     mdb_database: string = process.env.MDB_DATABASE as string;
     mdb_users: string = process.env.MDB_USERS as string;
+    mdb_assignments: string = process.env.MDB_ASSIGNMENTS as string;
     uri: string = `mongodb+srv://${this.mdb_username}:${this.mdb_pass}`+
         `@${this.mdb_cluster}.ufb3z.mongodb.net/${this.mdb_database}`+
         `?retryWrites=true&w=majority`;
     user_collection: Collection<Document> | undefined;
+    assignment_collection: Collection<Document> | undefined;
     client = new MongoClient(this.uri, { 
         useNewUrlParser: true, 
         useUnifiedTopology: true, 
@@ -25,6 +27,7 @@ class Server {
                 console.warn(err);
             } else {
                 this.user_collection = this.client.db(this.mdb_database).collection(this.mdb_users);
+                this.assignment_collection = this.client.db(this.mdb_database).collection(this.mdb_assignments);
                 console.log('connected to database.')
             }
         });
@@ -53,14 +56,35 @@ class Application {
         });
 
         this.app.get('/', (req, res) => {
-            return res.send('server listening...');
+            return res.send('ourfinals server listening...');
         });
 
-        this.app.post('/api/users/add', (req, res) => {
+        // USER ENDPOINTS
+        this.app.post('/users/add', (req, res) => {
             const userData = req.body;
             this.server.user_collection?.insertOne(userData);
             return res.sendStatus(200);
         });
+
+        this.app.get('/users/:username', async (req, res) => {
+            const username = req.params.username;
+            const user = await this.server.user_collection?.findOne({username: username});
+            return res.send(user);
+        });
+
+        // ASSIGNMENT ENDPOINTS
+        this.app.post('/assignments/add', (req, res) => {
+            const assignmentData = req.body;
+            this.server.assignment_collection?.insertOne(assignmentData);
+            return res.sendStatus(200);
+        });
+
+        this.app.get('/assignments/:id', async (req, res) => {
+            const id = new ObjectId(req.params.id);
+            const assignment = await this.server.assignment_collection?.findOne({ _id: id });
+            return res.send(assignment);
+        });
+
     }
 }
 
