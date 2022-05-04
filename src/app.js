4031df5f8dc50928bb3e0275031063eb3cbe668e
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-05-04 17:57:13 +0300

    remove typescript, setup heroku

--- src/app.js
+++ src/app.js
@@ -1,20 +1,19 @@
-import express from 'express';
+const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-import { Collection } from 'mongodb';
 require('dotenv').config('./env');
 
 class Server {
-    mdb_username: string = process.env.MDB_USERNAME as string;
-    mdb_pass: string = process.env.MDB_PASSWORD as string;
-    mdb_cluster: string = process.env.MDB_CLUSTER as string;
-    mdb_database: string = process.env.MDB_DATABASE as string;
-    mdb_users: string = process.env.MDB_USERS as string;
-    mdb_assignments: string = process.env.MDB_ASSIGNMENTS as string;
-    uri: string = `mongodb+srv://${this.mdb_username}:${this.mdb_pass}`+
+    mdb_username = process.env.MDB_USERNAME
+    mdb_pass = process.env.MDB_PASSWORD
+    mdb_cluster = process.env.MDB_CLUSTER
+    mdb_database = process.env.MDB_DATABASE
+    mdb_users = process.env.MDB_USERS
+    mdb_assignments = process.env.MDB_ASSIGNMENTS
+    uri = `mongodb+srv://${this.mdb_username}:${this.mdb_pass}`+
         `@${this.mdb_cluster}.ufb3z.mongodb.net/${this.mdb_database}`+
         `?retryWrites=true&w=majority`;
-    user_collection: Collection<Document> | undefined;
-    assignment_collection: Collection<Document> | undefined;
+    user_collection
+    assignment_collection
     client = new MongoClient(this.uri, { 
         useNewUrlParser: true, 
         useUnifiedTopology: true, 
@@ -22,7 +21,7 @@ class Server {
     });
 
     init() {
-        this.client.connect((err: Error) => {
+        this.client.connect((err) => {
             if(err) {
                 console.warn(err);
             } else {
@@ -41,7 +40,7 @@ class Server {
 class Application {
     server = new Server();
     app = express();
-    port = process.env.PORT || 3000
+    port = Number(process.env.PORT) || 3000
 
     init() {
         this.server.init()
@@ -62,26 +61,26 @@ class Application {
         // USER ENDPOINTS
         this.app.post('/users/add', (req, res) => {
             const userData = req.body;
-            this.server.user_collection?.insertOne(userData);
+            this.server.user_collection.insertOne(userData);
             return res.sendStatus(200);
         });
 
         this.app.get('/users/:username', async (req, res) => {
             const username = req.params.username;
-            const user = await this.server.user_collection?.findOne({username: username});
+            const user = await this.server.user_collection.findOne({username: username});
             return res.send(user);
         });
 
         // ASSIGNMENT ENDPOINTS
         this.app.post('/assignments/add', (req, res) => {
             const assignmentData = req.body;
-            this.server.assignment_collection?.insertOne(assignmentData);
+            this.server.assignment_collection.insertOne(assignmentData);
             return res.sendStatus(200);
         });
 
         this.app.get('/assignments/:id', async (req, res) => {
             const id = new ObjectId(req.params.id);
-            const assignment = await this.server.assignment_collection?.findOne({ _id: id });
+            const assignment = await this.server.assignment_collection.findOne({ _id: id });
             return res.send(assignment);
         });
 
